cad04b9abdee3be2372baee4e53e2ecce34c79ac
DecryptAG <akshat.goel.civ21@itbhu.ac.in>
2023-07-12 18:59:09 +0530

    unused components fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,8 @@
 import ImageGallery from "../components/ImageGallery"
 import Navbar from "../components/Navbar"
 import UploadForm from "../components/UploadForm"
-import { useAuth } from "../hooks/useAuth"
 
 const Home = () => {
-  const {user} = useAuth();
   return (
     <div className="max-w-4xl mx-auto">
       <Navbar/>
